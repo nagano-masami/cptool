db8385d63085352e5b504dfd5e9b0e88d8af643b
S8799 <nagano.masami@jgc.com>
2022-06-07 17:14:50 +0900

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.emit('chat message','ようそこチャットアプリへ');
+    socket.broadcast.emit('chat message','新しいユーザが接続しました。')
+    socket.on('disconnect',function(){
+      io.emit('chat message','あるユーザの接続が切れました')
+    })
+    socket.on('chat message', function(msg){
+        // console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
